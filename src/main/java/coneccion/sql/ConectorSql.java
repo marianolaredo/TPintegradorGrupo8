229310253047fc861cddd93ceaf2fbfa97e7a8e2
marianolaredo <mariano.laredo@gmx.com>
2023-04-26 15:59:50 -0300

    TPFinalArgentinaPrograma

---
+++ src/main/java/coneccion/sql/ConectorSql.java
@@ -0,0 +1,11 @@
+package coneccion.sql;
+
+public class ConectorSql {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/Pronosticos";
+
+    // Credenciales credentials
+    public static final String USER = "root";
+    public static final String PASS = "root";
+
+}
---
+++ src/main/java/coneccion/sql/package-info.java
@@ -0,0 +1,1 @@
+package coneccion.sql;
